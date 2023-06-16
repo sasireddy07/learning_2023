ef2ea169c1a070de5f21a3b54dea8afcaf3f4a4d
sasireddy07 <136625521+sasireddy07@users.noreply.github.com>
2023-06-16 17:58:03 +0530

    Add files via upload

---
+++ day2_qn1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void printExponent(double x) {
+    unsigned long long *ptr = (unsigned long long *)&x;
+    unsigned long long exponent = (*ptr >> 52) & 0x7FF;
+
+    // Print hexadecimal format
+    printf("Hexadecimal: 0x%llX\n", exponent);
+
+    // Print binary format
+    printf("Binary: 0b");
+    for (int i = 10; i >= 0; i--) {
+        printf("%d", (exponent >> i) & 1);
+    }
+    printf("\n");
+}
+
+int main() {
+    double x = 0.7;
+    printExponent(x);
+    return 0;
+}
